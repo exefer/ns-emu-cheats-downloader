89456ec08c8ad8d8ada4096728f4c8e89aa6d62c
exefer <jessieodeh08@gmail.com>
2026-01-20 18:11:08 +0100

    feat: implement GBAtemp cheat provider

--- src/cheat_provider/gbatemp.rs
+++ src/cheat_provider/gbatemp.rs
@@ -1,9 +1,36 @@
-use super::{CheatMap, CheatProvider};
+use super::{CheatMap, CheatProvider, parse_cheat_file};
+use crate::curl_helper::BodyExt;
+use curl::easy::Easy;
+use serde_json::Value;
+use std::collections::HashMap;
 
 pub struct GbaTempProvider;
 
 impl CheatProvider for GbaTempProvider {
-    fn get_cheats_for_title(&self, _title_name: &str, _title_id: &str) -> Option<CheatMap> {
-        None
+    fn get_cheats_for_title(&self, _title_name: &str, title_id: &str) -> Option<CheatMap> {
+        let mut easy = Easy::new();
+        let url = format!(
+            "https://api.github.com/repos/exefer/gbatemp-matias3ds-cheats/contents/titles/{}/cheats",
+            title_id
+        );
+        easy.url(&url).ok()?;
+        easy.useragent(env!("CARGO_PKG_NAME")).ok()?;
+        let data = easy.without_body().send_with_response::<Value>().ok()?;
+        let mut result = HashMap::new();
+
+        for item in data.as_array()? {
+            let build_id = {
+                let name = item["name"].as_str()?;
+                name.trim_end_matches(".txt").to_uppercase()
+            };
+            let url = item["download_url"].as_str()?;
+            easy.url(url).ok()?;
+            let content = easy.without_body().text().ok()?;
+            result
+                .entry(build_id)
+                .or_insert_with(Vec::new)
+                .extend(parse_cheat_file(&content));
+        }
+        Some(result)
     }
 }
